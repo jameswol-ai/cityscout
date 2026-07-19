da5a8dfac4ba13d35d9baeb88c9b639c5eec558d
James Wol <james.wol@petalmail.com>
2026-07-19 06:10:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,41 @@ import pandas as pd
 import polyline  # pip install polyline
 import math
 
+def decode_polyline(polyline_str):
+    """Decode a Google-encoded polyline string to a list of (lat, lon) tuples."""
+    if not polyline_str:
+        return []
+    index, lat, lng = 0, 0, 0
+    coordinates = []
+    length = len(polyline_str)
+
+    while index < length:
+        shift, result = 0, 0
+        while True:
+            b = ord(polyline_str[index]) - 63
+            index += 1
+            result |= (b & 0x1f) << shift
+            shift += 5
+            if b < 0x20:
+                break
+        dlat = ~(result >> 1) if (result & 1) else (result >> 1)
+        lat += dlat
+
+        shift, result = 0, 0
+        while True:
+            b = ord(polyline_str[index]) - 63
+            index += 1
+            result |= (b & 0x1f) << shift
+            shift += 5
+            if b < 0x20:
+                break
+        dlng = ~(result >> 1) if (result & 1) else (result >> 1)
+        lng += dlng
+
+        coordinates.append((lat / 1e5, lng / 1e5))
+    return coordinates
+
+
 # -------------------------
 # Configuration
 # -------------------------
